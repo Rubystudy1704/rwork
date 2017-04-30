8277799c3f569dbc449a0832fe6f6b70adc13b19
tk <kumi5638@yahoo.co.jp>
2017-04-30 19:05:07 +0900

    rails GW勉強会

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+
+  http_basic_authenticate_with name: "user", password: "secret", except: [:index, :show]
+
+
+  def new
+    TO do
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
